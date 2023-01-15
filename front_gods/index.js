bcc435b5654b051fbc029e3502b56d5a30fd42c5
George Aroshidze <leftcat9@gmail.com>
2023-01-15 16:46:21 +0400

    add index.js

---
+++ front_gods/index.js
@@ -0,0 +1,17 @@
+const x = 5 //number
+let y = 4
+
+const name = "giorgi" //string
+
+const isLoading = false
+
+let lastName; //undefined
+const nothing = null //null
+
+function numberMultiplayer(number){
+    console.log(number*5)
+}
+
+console.log(name)
+console.log(x + y)
+numberMultiplayer(67)
